00a7b15c60d7672f0732bb2387d9c87540ce6004
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-08-24 11:43:13 +0600

    Revert "ds 2"
    This reverts commit b358d2204bb5e0565fc8c365a87c3e4cebf1896a.

--- поток_Пример_ProDave/Program.cs
+++ поток_Пример_ProDave/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace поток_Пример_ProDave
@@ -11,16 +10,6 @@ namespace поток_Пример_ProDave
     {
         static void Main(string[] args)
         {
-            clDS ds = new clDS();
-            Thread thds = new Thread(ds.StartDS);
-            thds.Start();
-
-            while (true)
-            {
-                Thread.Sleep(5000);
-                Console.WriteLine("Main .....");
-            }
-
         }
     }
 }
