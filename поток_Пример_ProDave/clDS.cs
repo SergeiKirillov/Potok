42b3e575a4ed0f8593e81377114f97cf3a92c362
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-08-24 11:44:39 +0600

    Revert "Revert "ds 2""
    This reverts commit 00a7b15c60d7672f0732bb2387d9c87540ce6004.

---
+++ поток_Пример_ProDave/clDS.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace поток_Пример_ProDave
+{
+    class clDS
+    {
+        public void StartDS()
+        {
+            Thread ds70 = new Thread(dsPLC);
+            ds70.Start();
+
+            Thread ds80 = new Thread(dsSQL80);
+            ds80.Start();
+
+            Thread ds1c = new Thread(dsSQL1s);
+            ds1c.Start();
+
+            Thread dsMes = new Thread(dsMessage200);
+            dsMes.Start();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Start DS........");
+            }
+        }
+
+        private void dsPLC()
+        {
+            while (true)
+            {
+                Thread.Sleep(70);
+                Console.Write("70");
+            }
+            
+        }
+
+        private void dsSQL80()
+        {
+            while (true)
+            {
+                Thread.Sleep(80);
+                Console.Write("80");
+            }
+
+        }
+
+        private void dsSQL1s()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.Write("1C");
+            }
+
+        }
+
+        private void dsMessage200()
+        {
+            while (true)
+            {
+                Thread.Sleep(200);
+                Console.Write("200");
+            }
+
+        }
+    }
+}
