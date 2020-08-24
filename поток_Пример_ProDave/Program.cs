b358d2204bb5e0565fc8c365a87c3e4cebf1896a
SergeiKirillov <sergei.kirillov.a@gmail.com>
2020-08-24 11:36:34 +0600

    ds 2

--- поток_Пример_ProDave/Program.cs
+++ поток_Пример_ProDave/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace поток_Пример_ProDave
@@ -10,6 +11,16 @@ namespace поток_Пример_ProDave
     {
         static void Main(string[] args)
         {
+            clDS ds = new clDS();
+            Thread thds = new Thread(ds.StartDS);
+            thds.Start();
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+                Console.WriteLine("Main .....");
+            }
+
         }
     }
 }
